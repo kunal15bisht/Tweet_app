392d12a5192b3f2a6b0dc69f07e992ef8f66ce8f
kunal bisht <kunal15bisht@gmail.com>
2025-11-12 15:09:18 +0530

    Add user registration and login forms with enhanced styling.

--- Tweet/tweet_app/forms.py
+++ Tweet/tweet_app/forms.py
@@ -1,7 +1,63 @@
 from django import forms
 from .models import Tweet   
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, User
+
 
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
-        fields = ['text','photo']
+        fields = ['text', 'photo']
+
+    def __init__(self, *args, **kwargs):
+        super(TweetForm, self).__init__(*args, **kwargs)
+        self.fields['text'].widget.attrs.update({
+            'class': 'form-control form-control-lg',  # larger input size
+            'placeholder': 'What’s happening?',
+            'rows': 4,
+            'style': 'font-size: 1.1rem; padding: 15px; border-radius: 12px;'
+        })
+        self.fields['photo'].widget.attrs.update({
+            'class': 'form-control form-control-lg',
+            'style': 'padding: 10px; border-radius: 12px;'
+        })
+        self.fields['photo'].required = False
+
+
+
+class UserRegistrationsForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter your email',
+    }))
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationsForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Enter your username',
+        })
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Enter your password',
+        })
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Confirm your password',
+        })
+
+
+class UserLoginForm(forms.Form):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter your email',
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter your password',
+    }))
+
