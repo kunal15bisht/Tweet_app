9c434f6590f9d11b82c9228e0285cbad17c1d3c7
kunal bisht <kunal15bisht@gmail.com>
2025-11-15 19:56:48 +0530

    Add tweet_detail path to urlpatterns for individual tweet access.

--- Tweet/tweet_app/urls.py
+++ Tweet/tweet_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('tweet_home/', views.tweet_list, name='tweet_list'),
+    path('<int:tweet_id>/', views.tweet_detail, name='tweet_detail'),
     path('create/', views.tweet_create, name='tweet_create'),
     path('<int:tweet_id>/edit', views.tweet_edit, name='tweet_edit'),
     path('<int:tweet_id>/delete', views.tweet_delete, name='tweet_delete'),
