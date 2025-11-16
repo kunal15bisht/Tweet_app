cc846bfb111a66d56f34ddeb2672b554b7c4547e
kunal bisht <kunal15bisht@gmail.com>
2025-11-16 13:28:02 +0530

    Remove default ACL setting for AWS S3 and delete unused image files.

--- Tweet/Tweet/settings.py
+++ Tweet/Tweet/settings.py
@@ -154,7 +154,6 @@ if 'DATABASE_URL' in os.environ: # 'DATABASE_URL' is a default Render var
 
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_FILE_OVERWRITE = False
-    AWS_DEFAULT_ACL = 'public-read' # Make files public by default
 
     # This tells Django to use S3 for all file storage
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
